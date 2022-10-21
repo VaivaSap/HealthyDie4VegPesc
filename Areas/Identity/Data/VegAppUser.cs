9d86c78de356b69395efeb3bcc1b2d69bba80242
LAPTOP-EG9I2JDH\vaiva <vaiva.sapetkaite@gmail.com>
2022-10-21 15:16:42 +0300

    Add project files.

---
+++ Areas/Identity/Data/VegAppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VegApp.Areas.Identity.Data
+{
+    public class VegAppUser : IdentityUser<Guid>
+    {
+        public string DietPreference { get; set; }
+
+        public GenderEnum Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Weight { get; set; }
+    }
+}
+
+
