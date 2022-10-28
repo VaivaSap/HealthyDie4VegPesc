a38ea5da10e4b46605882c3f034843affd4de63e
katrina2704 <katrina.briede@gmail.com>
2022-10-28 20:40:06 +0300

    Update

---
+++ Pages/products/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VegApp.Data;
+using VegApp.Entities;
+
+namespace VegApp.Pages.products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly VegApp.Data.VegAppContext _context;
+
+        public IndexModel(VegApp.Data.VegAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
