4c8018e46273c38cee60e4ec3720a2c547e7de62
katrina2704 <katrina.briede@gmail.com>
2022-10-28 12:53:15 +0300

    Add project files.

---
+++ Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using VegApp.Areas.Identity.Data;
+
+namespace VegApp.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+
+        public decimal ProteinsIn100g { get; set; }
+        public decimal CarbsIn100g { get; set; }
+        public decimal FatsIn100g { get; set; }
+
+        public decimal CaloriesIn100g { get; set; }
+
+        public VegAppUser VegAppUser { get; set; }
+
+
+    }
+}
---
+++ Pages/Products/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VegApp.Data;
+using VegApp.Entities;
+
+namespace VegApp.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly VegApp.Data.VegAppContext _context;
+
+        public DetailsModel(VegApp.Data.VegAppContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
