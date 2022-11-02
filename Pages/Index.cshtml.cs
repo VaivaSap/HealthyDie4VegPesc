ebe05c935991b823d0b5600415d6b3fc04151977
LAPTOP-EG9I2JDH\vaiva <vaiva.sapetkaite@gmail.com>
2022-11-02 11:04:44 +0200

    I hope this version will work for you

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using VegApp.Data;
+using VegApp.Entities;
 
 namespace VegApp.Pages
 {
@@ -9,14 +12,34 @@ namespace VegApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly VegAppContext _vegAppContext;
+
+        public Product SelectedProduct { get; set; }
+
+
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+
+        public IndexModel(ILogger<IndexModel> logger, VegAppContext vegAppContext)
         {
             _logger = logger;
+            _vegAppContext = vegAppContext;
         }
+
+
+
+
 
         public void OnGet()
         {
 
+            Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid id);
+            Products = _vegAppContext.Products.Where(u => u.VegAppUser.Id == id).ToList();
+
         }
+
+
     }
 }
+
