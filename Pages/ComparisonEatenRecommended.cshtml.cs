2b35df26924bb937bba8b0f23a18d1d54226fada
LAPTOP-EG9I2JDH\vaiva <vaiva.sapetkaite@gmail.com>
2022-11-03 23:02:04 +0200

    Working on Results page// Counting nutritional values (still not comparing with recommended nutritional values)

--- Pages/ComparisonEatenRecommended.cshtml.cs
+++ Pages/ComparisonEatenRecommended.cshtml.cs
@@ -12,11 +12,26 @@ namespace VegApp.Pages
 
         private readonly VegAppContext _vegAppContext;
 
-
+        [BindProperty]
         public decimal DailyProteins { get; set; }
-        public decimal DailyCarbs { get; set; }  
+        [BindProperty]
+        public decimal DailyCarbs { get; set; }
+        [BindProperty]
         public decimal DailyFats { get; set; }
+        [BindProperty]
         public decimal DailyCalories { get; set; }
+        [BindProperty]
+
+        public decimal RecommendedDailyProteins { get; set; }
+        [BindProperty]
+
+        public decimal RecommendedDailyFats { get; set; }
+
+        [BindProperty]
+        public decimal RecommendedDailyCarbs { get; set; }
+        [BindProperty]
+        public decimal RecommendedDailyCalories { get; set; }
+        [BindProperty]
 
         public VegAppUser VegAppUser { get; set; }
 
@@ -32,6 +47,12 @@ namespace VegApp.Pages
 
             //Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid id);
             //Products = _vegAppContext.Products.Where(u => u.VegAppUser.Id == id).ToList();
+
+
+            DailyProteins = _vegAppContext.EatenProducts.Sum(e => e.Product.ProteinsIn100g * e.Amount / 100);
+            DailyCarbs = _vegAppContext.EatenProducts.Sum(e => e.Product.CarbsIn100g * e.Amount / 100);
+            DailyFats = _vegAppContext.EatenProducts.Sum(e => e.Product.FatsIn100g * e.Amount / 100);
+            DailyCalories = _vegAppContext.EatenProducts.Sum(e => e.Product.CaloriesIn100g * e.Amount / 100); 
 
         }
 
